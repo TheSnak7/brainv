6e738727fdf48062719ff4d19721453fb70db054
TheSnak7 <51061669+TheSnak7@users.noreply.github.com>
2025-04-08 19:41:33 +0200

    Load program from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{env, fs, path::Path};
+
 fn main() {
-    println!("Hi from brainv!");
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2);
+
+    let program_path = &args[1];
+
+    let program_text = fs::read_to_string(Path::new(program_path)).expect("Failed to read the file");
+
+    println!("BF Program\n =======\n{}\n=======", program_text);
 }
